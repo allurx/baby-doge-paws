c67b495446fdaa80f6c03d91f2082e4fcf1e5042
allurx <allurx.zyc@gmail.com>
2024-08-22 11:42:29 +0800

    添加todo清单

--- src/main/java/red/zyc/babydogepaws/common/TODO.java
+++ src/main/java/red/zyc/babydogepaws/common/TODO.java
@@ -3,6 +3,8 @@ package red.zyc.babydogepaws.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import red.zyc.babydogepaws.common.constant.Constants;
+import red.zyc.babydogepaws.core.BabyDogePawsApi;
+import red.zyc.babydogepaws.model.User;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.function.Supplier;
 
 import static red.zyc.toolkit.json.Json.JACKSON_OPERATOR;
 
@@ -25,6 +28,28 @@ public class TODO {
     private static final ScheduledExecutorService EXECUTOR_SERVICE = new ScheduledThreadPoolExecutor(Integer.MAX_VALUE, new NamedThreadFactory("TODO", false));
     public final TODO TODO = this;
 
+
+    /**
+     * <ol>
+     *     <li>初始化时，不是所有用户都需要登录一下tg获取授权参数，
+     *     如果游戏登录信息表中，登录记录的修改时间没有超过1天的话（游戏那边好像是1天过期），
+     *     直接用上次的授权参数进行授权就行
+     *     </li>
+     *     <li>
+     *         同一个用户执行{@link BabyDogePawsApi#authorize(User)}方法应该与其它方法互斥，
+     *         因为其它方法都是依赖与authorize方法返回的access_token
+     *     </li>
+     *     <li>
+     *         添加一个game_user_config表，标记读取游戏用户时哪些用户不需要进行自动化，
+     *         一些重要的账户暂时还是手动操作比较好
+     *     </li>
+     *     <li>
+     *         {@link Poller#pollWhenMiss(Supplier)}优化
+     *     </li>
+     *     <li></li>
+     *     <li></li>
+     * </ol>
+     */
     public TODO() {
     }
 
